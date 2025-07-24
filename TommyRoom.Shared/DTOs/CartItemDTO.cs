14024ee795910a792c718d31462a534b0cde8df5
Tomas Gonzales Guerra <tommy95pro@outlook.com>
2025-07-23 22:19:48 -0500

    TemporalBooking v1 😁

--- TommyRoom.Shared/DTOs/CartItemDTO.cs
+++ TommyRoom.Shared/DTOs/CartItemDTO.cs
@@ -3,5 +3,10 @@
 public class CartItemDTO
 {
     public int RoomId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Photo { get; set; }
     public int Quantity { get; set; }
+    public int Capacity { get; set; }
+    public decimal PricePerNight { get; set; }
 }
