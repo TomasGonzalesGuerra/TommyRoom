bd57069e42f91199d2f37e0417430331db009035
pc <pc@ANTAREZ-01>
2026-04-13 12:57:30 -0500

    respons to nav and roomsection

--- TommyRoom.FrontEnd/wwwroot/js/main.js
+++ TommyRoom.FrontEnd/wwwroot/js/main.js
@@ -1,8 +1,19 @@
 ﻿window.scrollToElement = function (elementId) {
     const el = document.getElementById(elementId);
+
     if (el) {
-        const offset = 80; // altura del nav sticky
+        const offset = 80;
         const top = el.getBoundingClientRect().top + window.scrollY - offset;
         window.scrollTo({ top, behavior: 'smooth' });
     }
 };
+
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+
+    if (window.scrollY > 99) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
